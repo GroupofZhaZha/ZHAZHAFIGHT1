cfe044c8ff970d9fa490f9b103cab7f09abaf215
kunlin021 <kunlin@buffalo.edu>
2018-09-23 18:12:02 -0400

    Add the Volume control handler
    able to change valume from slider

---
+++ ZhaZhaFight/Assets/Scripts/VolumeHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeHandler : MonoBehaviour {
+
+    public Slider changeVolume;
+    public AudioSource backgroudnMusic;
+	// Update is called once per frame
+	void Update () {
+        backgroudnMusic.volume = changeVolume.value;
+	}
+}

702824582525cd4aac5c92f24be8836e6560ad3d
kunlin021 <kunlin@buffalo.edu>
2018-10-21 14:59:29 -0400

    SettingUpTimer
    set up a timer text and having it countdown from 30s to 0

--- ZhaZhaFight/Assets/Scripts/ButtonHandler.cs
+++ ZhaZhaFight/Assets/Scripts/ButtonHandler.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonHandler : MonoBehaviour {
-    
+
+    public GameObject PausePanelUI;
+
 	public void PlayGameButtonHandler()
     {
+        PlayerPrefs.SetInt("hp", 30);
+        PlayerPrefs.SetFloat("time", 30f);
+        PlayerPrefs.SetInt("gold", 1000);
         SceneManager.LoadScene("Game");
     }
+    public void backToGameScene(){
+        SceneManager.LoadScene("Game");
+    }
+    public void pauseButtonHandler(){
+        PausePanelUI.SetActive(true);
 
+    }
+    public void ResumeButtonHandler(){
+        PausePanelUI.SetActive(false);
+    }
     public void BackToMainMenuPanelButtonHandler()
     {
         SceneManager.LoadScene("main");
---
+++ ZhaZhaFight/Assets/Scripts/TimerCounter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class TimerCounter : MonoBehaviour {
+    
+    public  Text timerDisplay;
+    public float timeLeft;
+    public bool isPause = false;
+	// Use this for initialization
+	void Start () {
+        
+
+        timeLeft = PlayerPrefs.GetFloat("time");
+	}
+  
+    public void setPause(){
+        isPause = true;
+    }
+    public void setUnPause(){
+        isPause = false;
+    }
+    // Update is called once per frame
+    void Update () {
+        if (!isPause)
+        {
+            
+
+            timeLeft = PlayerPrefs.GetFloat("time");
+            //print("open time: " + PlayerPrefs.GetFloat("time"));
+            timeLeft -= Time.deltaTime;
+            string text = "Time: " + timeLeft.ToString("f0") + "s";
+            timerDisplay.text = text;
+            PlayerPrefs.SetFloat("time", timeLeft);
+            if (timeLeft <= 0f)
+            {
+                SceneManager.LoadScene("WildMonsters");
+                PlayerPrefs.SetFloat("time", 30f);
+            }
+
+        }
+	}
+
+}

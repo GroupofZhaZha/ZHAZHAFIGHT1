bce48458f08188c3bd260515e42fe1d1b3f58d9f
kunlin021 <kunlin@buffalo.edu>
2018-10-21 19:14:57 -0400

    Organizing Script
    organized them intofolders

---
+++ ZhaZhaFight/Assets/Scripts/PanelDisplay/WildMonsterSetting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class WildMonsterSetting : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        
+       
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        float timeLeft = PlayerPrefs.GetFloat("time");
+        timeLeft -= Time.deltaTime;
+        //string text = "Time: " + timeLeft.ToString("f0") + "s";
+        //timerDisplay.text = text;
+        PlayerPrefs.SetFloat("time", timeLeft);
+        if (timeLeft <= 0f)
+        {
+            SceneManager.LoadScene("WildMonsters");
+
+        }
+    }
+}

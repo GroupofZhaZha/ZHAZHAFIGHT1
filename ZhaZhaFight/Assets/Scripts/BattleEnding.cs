7439281aecced8cefba84f39d6a68d3751481081
Graydee Chen <zehuiche@buffalo.edu>
2018-10-21 20:11:59 -0400

    I have created a new scene for battlefield and added multiple scripts for monsters to search and fight each other.
    In the new scene, battlefield, i added a collection of monsters and 2 summon scripts, and 2 move scripts. 1 summon script and 1 move script are for ally and the other 2 are for enemy.

---
+++ ZhaZhaFight/Assets/Scripts/BattleEnding.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleEnding : MonoBehaviour {
+
+    private float countDown = 2f;
+    private bool done = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (done)
+        {
+            if (countDown <= 0)
+            {
+                SceneManager.LoadScene("Game");
+            }
+            countDown -= Time.deltaTime;
+            return;
+        } 
+        if (GameObject.FindGameObjectsWithTag("Ally").Length <= 0)
+        {
+            done = true;
+        }
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
+        {
+            done = true;
+        }
+
+
+
+    }
+}
---
+++ ZhaZhaFight/Assets/Scripts/MySummons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySummons : MonoBehaviour {
+
+    public static Transform[] locations;
+    public GameObject go;
+
+    // Use this for initialization
+    void Start()
+    {
+        locations = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            locations[i] = transform.GetChild(i);
+            GameObject temp = go.transform.GetChild(1).gameObject;
+            GameObject monster = (GameObject)Instantiate(temp, locations[i].position, locations[i].rotation);
+            monster.transform.localScale = new Vector3(5f, 5f, 5f);
+            monster.transform.tag = "Ally";
+            monster.AddComponent<MyMonster>();
+        }
+
+    }
+}

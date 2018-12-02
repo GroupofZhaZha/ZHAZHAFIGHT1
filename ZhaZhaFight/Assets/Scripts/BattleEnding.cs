c092fb81af0b16b852e0d032f1ff9a2ab0b5ca5c
kunlin021 <kunlin@buffalo.edu>
2018-12-02 18:12:29 -0500

    Displaying AI Hp
    Ai's HP are being displayed under the bot scene and will be able to update after every battle

--- ZhaZhaFight/Assets/Scripts/BattleEnding.cs
+++ ZhaZhaFight/Assets/Scripts/BattleEnding.cs
@@ -2,21 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BattleEnding : MonoBehaviour {
 
     private float countDown = 2f;
     private bool done = false;
-
+    private bool end = false;
+    public Text result;
+    public GameObject EndPanel;
 
 	// Use this for initialization
 	void Start () {
-		
+        //EndPanel.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+
+        if (end){
+            done = false;
+            EndPanel.SetActive(true);
+            return;
+        }
         if (done)
         {
             if (countDown <= 0)
@@ -25,19 +33,34 @@ public class BattleEnding : MonoBehaviour {
                 SceneManager.LoadScene("Game");
                 PlayerPrefs.SetFloat("time", 20);
                 botMonsterController.resetBotMonster();
+
             }
             countDown -= Time.deltaTime;
             return;
         } 
+
         if (GameObject.FindGameObjectsWithTag("Ally").Length <= 0)
         {
             done = true;
+            int playerhp = PlayerPrefs.GetInt("hp");
+            PlayerPrefs.SetInt("hp", playerhp - GameObject.FindGameObjectsWithTag("Enemy").Length);
+
         }
         if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
         {
             done = true;
+            int aihp = PlayerPrefs.GetInt("aihp");
+            //PlayerPrefs.SetInt("aihp", aihp - GameObject.FindGameObjectsWithTag("Ally").Length);
         }
-
+        if(PlayerPrefs.GetInt("aihp")==0){
+            end = true;
+            result.text = "Victory";
+        }
+    
+        if(PlayerPrefs.GetInt("hp")==0){
+            end = true;
+            result.text = "Defeated";
+        }
 
 
     }
